a3c6875e1cff7f3f5008fd034748438a94ecbdac
Victor Oliveira <victorfelipe1608@gmail.com>
2022-12-21 20:49:20 -0300

    feat: Exercise 2 with JSX

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,57 +1,48 @@
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' }
-  ],
-  ativa: true
-};
-
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' }
-  ],
-  ativa: false
-};
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766']
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59']
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786']
+  }
+];
 
 const App = () => {
-  const dados = luana;
-  const sumCompras = dados.compras
-    .map((item) => Number(item.preco.replace('R$ ', '')))
-    .reduce((a, b) => a + b);
-
   return (
-    <div>
-      <p>
-        <b>Nome: </b> {dados.cliente}
-      </p>
-      <p>
-        <b>Idade: </b>
-        {dados.idade}
-      </p>
-      <p>
-        <b>Situação: </b>
-        <span style={{ color: !dados.ativa ? 'red' : 'green ' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>
-        <b>Total gasto: </b>
-        R$ {sumCompras}
-      </p>
-      {sumCompras > 10000 ? <p>Você está gastando muito!</p> : ''}
-    </div>
+    <section>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace('R$ ', '')) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <span>
+              <b>Preço: </b>
+              {produto.preco}
+            </span>
+            <ul>
+              {produto.cores.map((cor) => (
+                <li key={cor} style={{ backgroundColor: cor, color: '#fff' }}>
+                  {cor}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </section>
   );
 };
 
