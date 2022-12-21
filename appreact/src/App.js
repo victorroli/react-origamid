87e3fa7b6d652f84bf6976f89742d06937b3beb4
Victor Oliveira <victorfelipe1608@gmail.com>
2022-12-20 21:31:23 -0300

    feat: Testing jsx

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Minha aplicação de Teste</div>;
+  return (
+    <a
+      style={{ color: 'red', textDecoration: 'none', fontSize: '2rem' }}
+      href="https://origamid.com"
+      title="Isso é um site"
+    >
+      Origamid
+    </a>
+  );
 };
 
 export default App;
